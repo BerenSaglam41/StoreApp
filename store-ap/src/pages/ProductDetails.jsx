94692b1c1cce5ab1fe8a758f3a1c8f0ee3bcab19
hp <mustafaberensaglam9@gmail.com>
2025-04-26 13:31:03 +0300

    Redux END --

--- store-ap/src/pages/ProductDetails.jsx
+++ store-ap/src/pages/ProductDetails.jsx
@@ -1,46 +1,31 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useParams } from 'react-router'
 import ProductItem from '../compoments/ProductItem';
 import Loading from '../compoments/Loading';
-import requests from '../api/ApiClient';
 import { useDispatch, useSelector } from 'react-redux';
-import { setCart } from './Cart/cartSlice';
+import { addItemToCart } from './Cart/cartSlice';
+import { fetchProductById, selectProductById } from './catalog/catalogSlice';
 
 const ProductDetails = () => {
   const {id} = useParams();
-  const [loading,setLoading] = useState(true);
-  const [isAdding,setIsAdding] = useState(false);
-  const [product,setProduct] = useState(null);
-  const { cart } = useSelector((state) => state.cart);
+  const { cart,status } = useSelector((state) => state.cart);
   const dispatch = useDispatch()
+  const product = useSelector(state => selectProductById(state,id));
+  const { status: loading } = useSelector(state => state.catalog);
+
   const cartItem = cart?.cartItems.find(
     (i) => i.product.productId == product?.id
   );
 
   function handleAddItem (productId) {
-    setIsAdding(true);
-    requests.cart.addItem(productId)
-      .then(cart => dispatch(setCart(cart)))
-      .catch(err => console.log(err))
-      .finally(()=>setIsAdding(false));
+    dispatch(addItemToCart({productId:productId}));
   }
 
   useEffect(()=>{
-    async function fetchProductDetails () {
-      try{
-        const data = await requests.products.details(id)
-        setProduct(data);
-      }
-      catch(error){
-        console.log(error);
-      }
-      finally{
-        setLoading(false);
-      }
-    }
-    fetchProductDetails();
+    if(!product && id) dispatch(fetchProductById(id));
   },[id])
-  if(loading){
+
+  if(loading === "pendingFetchProductById"){
     return(<Loading message='Yükleniyor ...'/>)
   }
   if(!product) return (<h1>Ürün Bulunamadı</h1>)
@@ -48,7 +33,7 @@ const ProductDetails = () => {
     product={product} 
     handleAddItem={handleAddItem} 
     cartItem={cartItem}
-    isAdding={isAdding}
+    isAdding={status=="pendingAddItem"+product.id}
     />)
 }
 
